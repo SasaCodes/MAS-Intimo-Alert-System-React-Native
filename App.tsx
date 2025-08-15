505dc1d032dbd230acdddf2770f497e4928976f6
Nishen Sasanka <nishensasanka99@gmail.com>
2025-08-15 23:03:25 +0530

    a

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ export default function App() {
   return (
     
   <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Navigator initialRouteName="LoginScreen" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="SignupScreen" component={SignupScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
